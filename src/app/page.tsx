d714001449b8f5c1677379e888e2cc2c39840f08
TheDummi <raydecrom@hotmail.com>
2025-09-06 22:37:01 +0300

    updated conform with app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 /** @format */
 
-import Link from 'next/link';
-import NPM from '@/../public/icons/npm.svg';
 import API from '@/../public/icons/api.svg';
 import Dashboard from '@/../public/icons/dashboard.svg';
 import Github from '@/../public/icons/github.svg';
+import Link from 'next/link';
+import NPM from '@/../public/icons/npm.svg';
 
 export default function Home() {
 	return (
 		<>
 			<div>
-				<img src="logos/purple.png" className="banner" />
+				<img src='logos/purple.png' className='banner' />
 				<p></p>
 
-				<section id="about" className="ml-[10rem] mr-[10rem] mb-[5rem]">
+				<section id='about' className='ml-[10rem] mr-[10rem] mb-[5rem]'>
 					<h1>About</h1>
 					<p>
 						Xernerx Studios is a developer-driven project dedicated to building <strong>open-source tools and services</strong> for Discord bot creators. Our work focuses on making development easier,
@@ -29,60 +29,57 @@ export default function Home() {
 					</p>
 				</section>
 
-				<div className="grid-container">
-					<div className="card card-content">
+				<div className='grid-container'>
+					<div className='card card-content'>
 						<h2>Services</h2>
-						<div className="card-content">
-							<div className="flex justify-between mt-2 mb-2">
+						<div className='card-content'>
+							<div className='flex justify-between mt-2 mb-2'>
 								Statistics Dashboard
-								<p className="flex gap-3">
-									<Link href="https://app.xernerx.com">
-										<Dashboard className="icon" />
+								<p className='flex gap-3'>
+									<Link href='https://app.xernerx.com'>
+										<Dashboard className='icon' />
 									</Link>
 								</p>
 							</div>
 
-							<div className="flex justify-between mt-2 mb-2">
+							<div className='flex justify-between mt-2 mb-2'>
 								Statistics API
-								<p className="flex gap-3">
-									<Link href="https://api.xernerx.com">
-										<API className="icon" />
+								<p className='flex gap-3'>
+									<Link href='https://api.xernerx.com'>
+										<API className='icon' />
 									</Link>
 								</p>
 							</div>
 						</div>
 					</div>
-					<div className="card card-content">
+					<div className='card card-content'>
 						<h2>Software</h2>
-						<div className="card-content">
-							<div className="flex justify-between mt-2 mb-2">
+						<div className='card-content'>
+							<div className='flex justify-between mt-2 mb-2'>
 								Xernerx Framework
-								<p className="flex gap-3">
-									<Link href="https://www.npmjs.com/package/xernerx">
-										<NPM className="icon" />
-									</Link>
-									<Link href="https://github.com/xernerx/xernerx">
-										<Github className="icon" />
+								<p className='flex gap-3'>
+									<Link href='https://www.npmjs.com/package/xernerx'>
+										<NPM className='icon' />
 									</Link>
 								</p>
 							</div>
 
-							<div className="flex justify-between mt-2 mb-2">
+							<div className='flex justify-between mt-2 mb-2'>
 								Xernerx Stats
-								<p className="flex gap-3">
-									<Link href="https://www.npmjs.com/package/xernerx-stats">
-										<NPM className="icon" />
+								<p className='flex gap-3'>
+									<Link href='https://www.npmjs.com/package/xernerx-stats'>
+										<NPM className='icon' />
 									</Link>
-									<Link href="https://github.com/xernerx/xernerx-stats">
-										<Github className="icon" />
+									<Link href='https://github.com/xernerx/xernerx-stats'>
+										<Github className='icon' />
 									</Link>
 								</p>{' '}
 							</div>
 						</div>
 					</div>
-					<div className="card card-content">
+					<div className='card card-content'>
 						<h2>Discord</h2>
-						<p className="card-content"></p>
+						<p className='card-content'></p>
 					</div>
 				</div>
 			</div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,29 @@
 /** @format */
 'use client';
 
-import Link from 'next/link';
-
-import Icon from '@/../public/icons/icon.svg';
-import Home from '@/../public/icons/home.svg';
-import Cloud from '@/../public/icons/cloud.svg';
 import Dashboard from '@/../public/icons/dashboard.svg';
-import Api from '@/../public/icons/api.svg';
+import Home from '@/../public/icons/home.svg';
+import Icon from '@/../public/icons/icon.svg';
+import Link from 'next/link';
 
 export default function Header() {
 	return (
 		<>
-			<header className="rainbow">
+			<header className='rainbow'>
 				<div>
-					<Link href="https://xernerx.com">
-						<button title="Xernerx Studios">
-							<Icon className="icon text-violet-300" />
+					<Link href='https://xernerx.com'>
+						<button title='Xernerx Studios'>
+							<Icon className='icon text-violet-300' />
 						</button>
 					</Link>
-					<Link href="/">
-						<button title="Home">
-							<Home className="icon" />
+					<Link href='/'>
+						<button title='Home'>
+							<Home className='icon' />
 						</button>
 					</Link>
-					<Link href="https://app.xernerx.com">
-						<button title="Dashboard">
-							<Dashboard className="icon" />
-						</button>
-					</Link>
-					<Link href="https://api.xernerx.com">
-						<button title="API Documentation and endpoint">
-							<Api className="icon" />
-						</button>
-					</Link>
-					<Link href="https://xernerx.com/hosting">
-						<button title="Hosting">
-							<Cloud className="icon" />
+					<Link href='https://app.xernerx.com'>
+						<button title='Dashboard'>
+							<Dashboard className='icon' />
 						</button>
 					</Link>
 				</div>
