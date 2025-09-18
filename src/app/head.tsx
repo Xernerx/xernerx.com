416725effd271f65a1a2f887e11ba5b2996330be
TheDummi <raydecrom@hotmail.com>
2025-09-18 15:34:58 +0200

    ads?

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+/** @format */
+
+// app/head.tsx
+export default function Head() {
+	return (
+		<>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4242117068384423" crossOrigin="anonymous"></script>
+		</>
+	);
+}
